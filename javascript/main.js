96af2e54dd3eb5bc16f3a08abaffbee5a2ae1d31
Janell Forchu <jforchu@gmail.com>
2023-06-21 11:36:17 -0400

    audio changes

--- javascript/main.js
+++ javascript/main.js
@@ -2,8 +2,10 @@ let musicPlayer = document.getElementById("myAudio");
 
 function playAudio() { 
   musicPlayer.play(); 
+  musicPlayer.addEventListener(click,playAudio);
 } 
 
 function pauseAudio() { 
   musicPlayer.pause(); 
+  musicPlayer.addEventListener(click,pauseAudio);
 } 
